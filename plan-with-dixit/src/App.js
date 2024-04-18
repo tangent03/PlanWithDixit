f4248eb55602d525ccf134ff16cff7a239dc622a
tangent03 <amanawp03@gmail.com>
2024-04-18 11:54:09 +0530

    first commit

---
+++ plan-with-dixit/src/App.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import data from './data'
+import Tours from "./components/Tours";
+
+const App = () => {
+
+  useEffect(() => {
+    document.title = "Plan With Dixit";
+  }, [])
+
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0) {
+    return (
+        <div className="refresh">
+          <h2>No Tours Left</h2>
+          <button className="btn-white" onClick={() => setTours(data)}>
+            Refresh
+          </button>
+        </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
+
+
+//Learnings From This project
